170758739405e4465184d344ec3b44eb5f528658
Repo-Testing-Account <btechagri2021@gmail.com>
2026-07-09 23:57:31 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Version 1" in response.data
+    assert b"Version 2" in response.data

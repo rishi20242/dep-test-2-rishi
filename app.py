88292933d13ad0a87b57b67c0581f48186371387
Repo-Testing-Account <btechagri2021@gmail.com>
2026-07-13 04:43:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Version 2"
+    return "Hello from Version 1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
